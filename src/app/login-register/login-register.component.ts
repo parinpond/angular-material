57e45bd4073192fc414b5a2a56f628c78b556ad7
parinpond <parinpond.lag@gmail.com>
2021-09-30 21:43:36 +0700

    (feat): create function checkLogin

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+var _ = require('lodash');
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.component.html',
   styleUrls: ['./login-register.component.scss']
 })
 export class LoginRegisterComponent implements OnInit {
-
+  userData:any={
+    username:'',
+    password:''
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+  checkLogin(){
+    if(!_.isEmpty(this.userData.username)&&!_.isEmpty(this.userData.password)){
 
+    }
+  }
 }
